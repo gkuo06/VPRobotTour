09a74f730cff1973b012084df65c8c3b21740369
gkuo06 <garokuo@gmail.com>
2023-12-19 02:34:48 -0800

    Perform basic calculations to determine which coordinate of each checkpoint is closes to actual path.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import math
 
-def create_grid_graph(size, edge, checkpoints):
+def create_grid_graph(size, edge=None, checkpoints=None):
     G = nx.grid_2d_graph(size, size)
 
-    #Can remove edges here to represent walls, but I forgot how to do that lol
-    #Add the part about checkpoints to this and make the conditional a compound conditional
+    # Can remove edges here to represent walls, but I forgot how to do that lol
+    # Add the part about checkpoints to this and make the conditional a compound conditional
     if edge is not None:
         for coordinate in edge:
             G.remove_edge(coordinate[0][0], coordinate[0][1])
@@ -22,9 +23,9 @@ def draw_grid(G, path=None):
     for node in G.nodes():
         x, y = node
         if ((x // 3) + (y // 3)) % 2 == 0:
-            color_map.append('orange')  # Replace 'color1' with your ORANGE
+            color_map.append('orange')  # Replace 'color1' with ORANGE
         else:
-            color_map.append("lightblue")  # Replace 'color2' with another LIGHT BLUE
+            color_map.append("lightblue")  # Replace 'color2' with LIGHT BLUE
 
     nx.draw(G, pos, with_labels=True, node_color=color_map, node_size=600)
 
@@ -35,36 +36,57 @@ def draw_grid(G, path=None):
 
     plt.show()
 
-#The point is that path contains a list of tuples that represent the coordinates of the path it takes
-#False will represent the y-axis, True will represent the x-axis
-def calc_turns(path, num_turn=0, temp=False):
+# The point is that path contains a list of tuples that represent the coordinates of the path it takes
+# False will represent the y-axis, True will represent the x-axis
+def calc_turns(path, num_turn=0):
     axis = path[0][1] == path[1][1]
 
     for i in range(1,len(path)-1):
         temp = path[i][1] == path[i+1][1]
         if temp is not  axis:
-            num_turn+=1
-            axis=temp
+            num_turn += 1
+            axis = temp
         print(f"Coordinates of node {i}: {path[i]}")
+    print(f"Moved: {len(path)-1}")
     print(f"Turned: {num_turn}")
 
-def calc_weight(path, turns=None, turn_time=4, travel_time=1):
-    ...
+# Maybe use optimization to determine which gateways to shoot for? Probably should eliminate one gateway at least...
+# Idea is to find the coordinate of the node in each checkpoint that is closest to the most optimal path.
+# HAS NOT BEEN TESTED YET 12/18/23
+def calc_weight(start, end, edge, checkpoints, turns=None, turn_time=4, travel_time=1):
+    G = create_grid_graph(12, edge)
 
-def createGrid(start_x=1, start_y=0, end_x=10, end_y=10, edge=None, checkpoints=None):
-    #Initialize the graph
+    path = nx.shortest_path(G, source=start, target=end, method='dijkstra')
+
+    # Index = Checkpoint_# - 1
+    nearest_checkpoint_coords = []
+
+    # Should in theory add all the nearest checkpoint coords to the list
+    for checkpoint in checkpoints:
+        min_distance = float("inf")
+        for coord1 in checkpoint:
+            # DISTANCE FORMULAAAAAAAAAAAAAA
+            for coord2 in path:
+                distance = math.sqrt((coord1[0] - coord2[0])**2 + (coord1[1] - coord2[1])**2)
+                if distance < min_distance:
+                    min_distance = distance
+                    nearest_checkpoint_coords.append(coord1)
+    print(nearest_checkpoint_coords)
+
+def createGrid(start=(0,1), end=(10,10), edge=None, checkpoints=None):
+    # Initialize the graph
     G = create_grid_graph(12, edge, checkpoints)
 
-    #Find the shortest path from the start point to the end node
-    path = nx.shortest_path(G, source=(int(start_y), int(start_x)), target=(int(end_y), int(end_x)), method='dijkstra')
+    # Find the shortest path from the start point to the end node
+    path = nx.shortest_path(G, source=start, target=end, method='dijkstra')
 
     calc_turns(path)
 
-    #Draw it
+    # Draw it using the below
     draw_grid(G, path)
 
 def main():
-    #List of coordinates of all the edges of the grid
+    # List of coordinates of all the edges of the grid
     edges_numbering = {
         "1" : [((0,2),(0,3)), ((1,2),(1,3)), ((2,2),(2,3))],
         "2" : [((0,5),(0,6)), ((1,5),(1,6)), ((2,5),(2,6))],
@@ -92,7 +114,7 @@ def main():
         "23" : [((8,6),(9,6)), ((8,7),(9,7)), ((8,8),(9,8))],
         "24" : [((8,9),(9,9)), ((8,10),(9,10)), ((8,11),(9,11))]
     }
-    #List of coordinates for all of the boxes in the track for a total of 16, each represented by a 3x3 node area
+    # List of coordinates for all of the boxes in the track for a total of 16, each represented by a 3x3 node area
     boxes_numbering = {
         "1" : {(0,0), (0,1), (0,2), (1,0), (1,1), (1,2), (2,0), (2,1), (2,2)},
         "2" : {(0,3), (0,4), (0,5), (1,3), (1,4), (1,5), (2,3), (2,4), (2,5)},
@@ -114,22 +136,25 @@ def main():
 
     if int(input("1 if default, 0 else: ")) == 1:
         createGrid()
+
     else:
         x,y = input("Enter the coordinates of the starting point: ").split(",")
+        starting_coords = (y,x)
         a,b = input("Enter the coordinates of the ending point: ").split(",")
+        ending_coords = (b,a)
 
         missing_edges = []
         for i in range(8):
             missing_edges.append(edges_numbering[input("Enter an edge that is missing: ")])
 
         checkpoints = []
-        for i in range(4):
+        for i in range(3):
             checkpoints.append(boxes_numbering[input("Enter the boxes that are checkpoints: ")])
 
         # Note to self when continuing: find a way to add all these edges to a list or smth and then create a for loop that graphs all the data from the dictionary COMPLETE
         # use: edges_numbering[edge_missing] as a parameter COMPLETE
-        #Next, find a way to add the checkpoints, the process will be similar to the edge process, but this will require adjusting the Dijkstra algorithm a little
-        createGrid(x, y, a, b, missing_edges, checkpoints)
+        # Next, find a way to add the checkpoints, the process will be similar to the edge process, but this will require adjusting the Dijkstra algorithm a little
+        createGrid(starting_coords, ending_coords, missing_edges, checkpoints)
 
 if __name__ == "__main__":
     main()
